54964d53be859fb4c12841d0f7445f0ad50d362e
small-lin-jam <linjunhui2012126@126.com>
2025-05-13 21:11:08 +0800

    8.3.7.5正式版
    删除一个危险命令
    新增安全声明
    对老旧电脑及部分性能占用较高（如打游戏等）对磁盘占用过大问题进行了修改，间接加快运行速度
    删除部分仅老旧电脑的系统缓存/临时文件清理命令
    用powershell来删除需要长时间操作的命令，加快运行速度
    用户体验优化（这条事实上没有什么用）

--- SCRT.bat
+++ SCRT.bat
@@ -3,8 +3,6 @@ setlocal
 color 0A&&mode con COLS=120 LINES=50
 setlocal enabledelayedexpansion
 chcp 936
-
-rem scan app
 echo ECHO:自检中……
 echo
 echo ECHO:正在检查程序运行目录……
@@ -22,42 +20,37 @@ if not exist "%systemdrive%\SCRT" (
 echo ECHO:自检完成！
 echo.
 echo 制作者：林先生
-echo 系统清理诊断程序[版本 8.3.6.6正式版]
+echo 系统清理诊断程序[版本 8.3.7.5正式版]
 echo 制作者：林先生。制作团队：Steven Lin Studio（上海木木个人工作室）。保留所有权利。
-echo 正版请联系linjunhui2012@hotmail.com获取正版！
-echo 本程序受个人版权保护，发现盗版，从严处理！
-echo 拒绝盗版，从我做起！
+echo 原版请联系linjunhui2012@hotmail.com获取！
 echo 本文件受个人知识版权保护，但已经开源，使用MIT开源协议，请访问https://github.com/small-lin-jam/SCRT/！
 echo ECHO:自检完成！
-
-rem set
 echo ECHO:初始化中……
 echo ECHO:设置中……
 echo ECHO:正在设置基础变量……
 set d=%~s0
-set v=8.3.6.6 正式版
+set v=8.3.7.5 正式版
 set name=系统清理诊断程序
 set fname=%~n0%
 set type=%~x0
 set SCRT="%cd%\%fname%%type%"
 set SFCD=%systemdrive%\SCRT\SFC.bat
+set cleanbat=%systemdrive%\SCRT\cb.bat
 set logs=%systemdrive%\SCRT\SCRT.log
 set set=%systemdrive%\SCRT\main.set
 set uns=%systemdrive%\SCRT\uns.bat
 set tc=%systemdrive%\SCRT\tempclean.bat
+set cls=cls^&^title %name%^&^ver^&^echo %name%[版本 %v%]^&^echo 制作者：林先生^&^echo.
 echo ECHO:基础变量设置完成！
 echo ECHO:正在设置错误报告变量……
 set su=F
-set tb=F
 set sfcs=F
 set mrts=F
-set wsdboerr=F
 set UACerr=F
 set ierr=F
 set nperr=F
 set cerr=F
 set serr=F
-set merr=F
 set mrterr=F
 echo ECHO:设置错误报告变量完成！
 echo ECHO:设置完成！
@@ -86,8 +79,6 @@ echo rd /s /q %temp% >nul 2>nul >>"%tc%"
 echo md %temp% >nul 2>nul >>"%tc%"
 echo exit >>"%tc%"
 echo ECHO:创建完成！
-
-rem logs
 echo ---------------------------------------------------------------------------------------------------- >"%logs%"
 echo SCRT-runbegintime:%date% %time% >>"%logs%"
 echo SCRT-vision:%v% >>"%logs%"
@@ -108,14 +99,10 @@ set start=%time%
 color 0A&&mode con COLS=120 LINES=50
 cls&&title %name%
 echo.
-
-rem initialization end
-
-rem information
 color 0A&&mode con COLS=120 LINES=50
 cls&&title 系统清理诊断程序[版本 %v%]
 echo ---------------------------------------------------------开源说明------------------------------------------------------
-echo 系统清理诊断程序[版本 8.3.6.6 正式版] 
+echo 系统清理诊断程序[版本 8.3.7.5 正式版] 
 echo The MIT License (MIT)
 echo.
 echo Copyright (C) 2025 StevenLin Studio
@@ -137,10 +124,12 @@ TIMEOUT /T 5
 color 0A&&mode con COLS=120 LINES=50
 cls&&title 系统清理诊断程序[版本 %v%]
 echo -----------------------------------------------------------说明--------------------------------------------------------
-echo 系统清理诊断程序[版本 8.3.6.6 正式版] 
+echo 系统清理诊断程序[版本 8.3.7.5 正式版] 
 echo 制作者：林先生。制作团队：Steven Lin Studio（上海木木个人工作室）。保留所有权利。
 echo 本说明林先生（制作者）保留其所有解释权！
 echo 本说明说明了所有可能存在纠纷或刑事的任何问题！
+echo.
+echo 安全声明：本程序部分操作会被误判为危险操作，可以查看日志文件和弹窗提示检查文件是否有病毒存在！
 echo.
 echo 调用软件：cmd.exe；taskschd.msc；ping.exe；cleanmgr；SFC.exe
 echo 调用软件版权：版权所有 (C) Microsoft Corporation。保留所有权利。
@@ -154,10 +143,10 @@ echo 警告：禁止在该工具中进行植入病毒等不正当行为！一经发现，严查！！！
 echo 如发现bug或植入病毒的情况可将bug信息和SCRT.log文件一起发送至linjunhui2012@hotmail.com！
 echo -----------------------------------------------------------------------------------------------------------------------
 TIMEOUT /T 5
-
-rem start set
+echo. >>"%logs%"
+echo set: >>"%logs%"
 color 0A&&mode con COLS=120 LINES=50
-cls&&title %name%&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.
+%cls%
 echo 开始运行！
 echo 温馨提示：没设置的项可以将"%systemdrive%\SCRT\main.set"删除
 echo.
@@ -170,12 +159,16 @@ if not exist "%set%" (
 		cleanmgr /sageset:5
 		echo 设置成功！
 		echo cleanmgrset:T >> "%set%"
+		echo [%date% %time%]cleanmgr:T >>"%logs%"
 	) else if "!ip!"=="n" (
 		echo 跳过设置过程成功！
 		echo cleanmgrset:F >> "%set%"
+		echo [%date% %time%]cleanmgr:F >>"%logs%"
 	 ) else (
 		echo 无效输入，请输入y或 n。
+		echo [%date% %time%]cleanmgr:ERROR >>"%logs%"
 	)
+	echo [%date% %time%]cleanmgr set >>"%logs%"
 	echo.
 	echo 警告：自启动扩展项为危险项，会关闭用户账户控制（简称UAC），您的
 	echo       电脑一旦被病毒感染将会破坏您的电脑，请谨慎开启（推荐与
@@ -184,42 +177,27 @@ if not exist "%set%" (
 	if "!ip!"=="y" (
 		reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d "0" /f
 		echo ECHO:正在启用自启动……
-		Schtasks /Create /SC ONLOGON /TN "SCRT" /TR "%SCRT%" /RL HIGHEST /F
+		Schtasks /Create /SC ONLOGON /TN "SCRT" /TR "%SCRT%" /F
 		echo ECHO:启用自启动完成！
 		if !errorlevel! == 1 (
 			set UACerr=T
 			echo WARNING:错误！
+			echo [%date% %time%]startup set error >>"%logs%"
 		) 
 		set su=T
 		echo 打开成功！
 		echo startup:T >> "%set%"
+		echo [%date% %time%]startup:T >>"%logs%"
 	) else if "!ip!"=="n" (
 		set su=F
 		echo 关闭成功！
 		echo startup:F >> "%set%"
+		echo [%date% %time%]startup:F >>"%logs%"
 	 ) else (
 		echo 无效输入，请输入y或 n。
+		echo [%date% %time%]startup:ERROR >>"%logs%"
 	)
-	echo.
-	echo 警告:此扩展项在部分性能不佳的电脑上会导致卡顿，会占用小部分CPU和内存
-	echo       请谨慎开启！
-	set /p ip=您想要让SCRT以高性能模式运行吗？（y/n）:
-	if "!ip!"=="y" (
-		reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v DisableBackoff /t REG_DWORD /d "1" /f
-		if !errorlevel! == 1 (
-			set wsdboerr=T
-			echo WARNING:注册表错误！
-		) 
-		set tb=T
-		echo 打开成功！
-		echo turboset:T >> "%set%"
-	) else if "!ip!"=="n" (
-		set tb=F
-		echo 关闭成功！
-		echo turboset:F >> "%set%"
-	) else (
-		echo 无效输入，请输入y或 n。
-	)
+	echo [%date% %time%]startup set >>"%logs%"
 	echo.
 	echo 恢复模式说明:每次运行SCRT时会自动尝试修复被恶意软件修改的
 	echo                系统文件，特别是中病毒或特殊情况导致文件损坏时！
@@ -229,6 +207,7 @@ if not exist "%set%" (
 		set sfcs=T
 		echo 打开成功！
 		echo sfcset:T >> "%set%"
+		echo [%date% %time%]sfc:T >>"%logs%"
 		echo.
 		echo 增强模式说明:通过mrt删除您电脑上的可疑文件，
 		echo               来保护您的电脑！如果恢复模式未开
@@ -243,12 +222,15 @@ if not exist "%set%" (
 			set mrts=T
 			echo 打开成功！
 			echo mrtset:T >> "%set%"
+			echo [%date% %time%]mrt:T >>"%logs%"
 		) else if "!ip!"=="n" (
 			set mrts=F
 			echo 关闭成功！
 			echo mrtset:F >> "%set%"
+			echo [%date% %time%]mrt:F >>"%logs%"
 		) else (
 			echo 无效输入，请输入y或 n。
+			echo [%date% %time%]mrt:ERROR >>"%logs%"
 		)
 	) else if "!ip!"=="n" (
 		set sfcs=F
@@ -256,77 +238,78 @@ if not exist "%set%" (
 		echo 关闭成功！
 		echo sfcset:F >> "%set%"
 		echo mrtset:F >> "%set%"
+		echo [%date% %time%]sfc:F >>"%logs%"
+		echo [%date% %time%]mrt:F >>"%logs%"
 	 ) else (
 		echo 无效输入，请输入y或 n。
+		echo [%date% %time%]sfc:ERROR >>"%logs%"
 	)
+	echo [%date% %time%]sfc^&mrt set >>"%logs%"
 	TIMEOUT /T 3
 ) else (
 	echo cleanmgr已设置
 	echo.
 	type "%set%" | findstr /c:"startup:T" >nul && set su=T || set su=F
 	echo 自启动已设置
-	echo.
-	type "%set%" | findstr /c:"turboset:T" >nul && set tb=T || set tb=F
-	echo 性能模式已设置
+	echo [%date% %time%]startup set >>"%logs%"
 	echo.
 	type "%set%" | findstr /c:"sfcset:T" >nul && set sfcs=T || set sfcs=F
 	echo 恢复模式已设置
+	echo [%date% %time%]sfc set >>"%logs%"
 	echo.
 	type "%set%" | findstr /c:"mrtset:T" >nul && set mrts=T || set mrts=F
+	echo [%date% %time%]mrt set >>"%logs%"
 	echo 增强模式已设置
 )
 echo.
-
-rem start
 color 0A&&mode con COLS=120 LINES=50
-cls&&title %name%&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.
-
-rem network
+%cls%
+echo. >>"%logs%"
+echo ---------------------------------------------------------------------------------------------------- >>"%logs%"
+echo network-ping: >>"%logs%"
+echo. >>"%logs%"
+echo ping#1:www.baidu.com >>"%logs%"
 echo 正在清理网络缓存……
 del "%userprofile%\Local Settings\Temporary Internet Files\*.*" /s /q /f >nul 2>nul
+echo [%date% %time%]Temporary Internet Files cleaned >>"%logs%"
 echo 清理完成！
 echo.
 echo 正在刷新DNS缓存……
 ipconfig /flushdns
+echo [%date% %time%]Temporary DNS cleaned >>"%logs%"
 echo 刷新缓存完成！
 echo.
 echo 正在测试网络连通性（www.baidu.com）……
 ping www.baidu.com
+echo [%date% %time%]ping www.baidu.com completed >>"%logs%"
 echo 网络连通性已测试完成！
 if !errorlevel! == 1 (
 	set nperr=T
+	echo [%date% %time%]ping error >>"%logs%"
 ) 
-
-rem network-ping log
-echo. >>"%logs%"
-echo network-ping: >>"%logs%"
-echo. >>"%logs%"
-echo ping#1:www.baidu.com >>"%logs%"
 echo. >>"%logs%"
 echo network-ping end >>"%logs%"
 echo. >>"%logs%"
 echo 网络测试已完成！
 TIMEOUT /T 2
-
-rem clean
-cls&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.&&echo 正在清理电脑垃圾文件……
-
-rem cleanlog
+%cls%&&echo 正在清理电脑垃圾文件……
 echo clean up directory: >>"%logs%"
 echo. >>"%logs%"
+echo files#1:%systemdrive%\*.tmp >>"%logs%"
+echo files#2:%systemdrive%\*.temp >>"%logs%"
+echo files#3:%systemdrive%\*.old >>"%logs%"
 echo directory#1:%windir%\KB*.log >>"%logs%"
 echo directory#2:%temp% >>"%logs%"
 echo directory#3:%windir%\Downloaded Program Files >>"%logs%"
 echo directory#4:%windir%\SoftwareDistribution\Download >>"%logs%"
 echo directory#5:%windir%\System32\LogFiles >>"%logs%"
 echo directory#6:%windir%\Help >>"%logs%"
-echo directory#7:%userprofile%\Downloads >>"%logs%"
-echo directory#8:%userprofile%\Cookies >>"%logs%"
-echo directory#9:%userprofile%\Recent >>"%logs%"
-echo directory#10:%userprofile%\AppData\Local\Microsoft\Windows\INetCache\IE >>"%logs%"
-echo directory#11:%systemdrive%\$WINDOWS.~BT >>"%logs%"
-echo directory#12:%systemdrive%\ProgramData\Microsoft\Windows\WER >>"%logs%"
-echo directory#13:%userprofile%\AppData\Local\CrashDumps >>"%logs%"
+echo directory#7:%userprofile%\Cookies >>"%logs%"
+echo directory#8:%userprofile%\Recent >>"%logs%"
+echo directory#9:%userprofile%\AppData\Local\Microsoft\Windows\INetCache\IE >>"%logs%"
+echo directory#10:%systemdrive%\$WINDOWS.~BT >>"%logs%"
+echo directory#11:%systemdrive%\ProgramData\Microsoft\Windows\WER >>"%logs%"
+echo directory#12:%userprofile%\AppData\Local\CrashDumps >>"%logs%"
 echo. >>"%logs%"
 echo clean up directory end >>"%logs%"
 echo. >>"%logs%"
@@ -335,18 +318,17 @@ echo.
 echo 后台清理中……
 echo.
 echo 正在启动cleanmgr……
-cleanmgr /sagerun:5 /autoclean
+start /min cleanmgr /sagerun:5 /autoclean
 echo.
 echo 正在自动清理……
-del %windir%\KB*.log /f /s /q >nul 2>nul
-del /f /s /q %systemdrive%\recycled\*.* >nul 2>nul
-del /f /s /q %windir%\*.bak  >nul 2>nul
+echo powershell -Command "Get-ChildItem 'C:\' -Include '*.old','*.tmp','*.temp' -Recurse -ErrorAction SilentlyContinue | Where-Object {$_.PSIsContainer -eq $false} | Remove-Item -Force -ErrorAction SilentlyContinue"^&^exit >%cleanbat%
+start /min %cleanbat%
+start  /min cmd /q /d /c "del %windir%\KB*.log /f /s /q"
+start  /min cmd /q /d /c "del /f /s /q %windir%\*.bak"
 del "%windir%\Downloaded Program Files\*.*" /s /q /f >nul 2>nul
-del "%windir%\SoftwareDistribution\Download\*.*" /s /q /f >nul 2>nul
 del "%windir%\System32\LogFiles\*.*" /s /q /f >nul 2>nul
 del "%windir%\Help\*.*" /s /q /f >nul 2>nul
 del %windir%\temp\*.* /s /q /f >nul 2>nul
-del %userprofile%\Downloads\*.* /s /q /f >nul 2>nul
 del %userprofile%\Cookies\*.* /s /q /f >nul 2>nul
 del %userprofile%\Recent\*.* /s /q /f >nul 2>nul
 del %userprofile%\AppData\Local\Microsoft\Windows\INetCache\IE\*.* /s /q /f >nul 2>nul
@@ -356,12 +338,12 @@ rd /s /q %windir%\SoftwareDistribution\Download\ >nul 2>nul
 md %windir%\SoftwareDistribution\Download\ >nul 2>nul
 rd /s /q %userprofile%\AppData\Local\CrashDumps >nul 2>nul
 md %userprofile%\AppData\Local\CrashDumps >nul 2>nul
+echo. >>"%logs%"
+echo [%date% %time%]files cleaned >>"%logs%"
 echo.
 echo 清理完成！
 TIMEOUT /t 2
-
-rem scan
-cls&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.&&echo 开始后台修复系统文件！
+%cls%&&echo 开始后台修复系统文件！
 echo @echo off >"%SFCD%"
 echo color 0A^&^mode con COLS=120 LINES=50 >>"%SFCD%"
 echo title SFC.bat >>"%SFCD%"
@@ -373,31 +355,22 @@ if %mrts% == T (
 	if !errorlevel! == 1 (
 		set mrterr=T
 		echo WARNING:mrt命令写入失败！
+		echo [%date% %time%]mrt error >>"%logs%"
 	) 
 )
 echo exit >>"%SFCD%"
 if %sfcs% == T (
 	start /min %SFCD%
+	echo [%date% %time%]sfc ran >>"%logs%"
 	echo 请等待sfc.bat自动修复完成
 	if !errorlevel! == 1 (
 		set serr=T
 		echo WARNING:%SFCD%打开失败！
+		echo [%date% %time%]sfc error >>"%logs%"
 	)
 	echo.
 	TIMEOUT /t 5
 )
-
-rem memory
-cls&&title %name%&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.
-echo 开始检查内存！
-systeminfo | findstr /I /B /C:"物理内存总量" /C:"可用的物理内存"
-if !errorlevel! == 1 (
-	set merr=T
-) 
-echo 内存检查完成！
-TIMEOUT /t 2
-
-rem runtime
 set end=%time%
 echo ECHO:结束计时！
 echo ECHO:正在计算时间……
@@ -416,26 +389,21 @@ if 1%ms% lss 100 set ms=0%ms%
 set /a totalsecs = %hours%*3600 + %mins%*60 + %secs%
 set runtime=%hours%:%mins%:%secs%.%ms% (%totalsecs%.%ms%s total)
 echo ECHO:计算完成！运行时间:%runtime%！
-
-rem logs
 echo ECHO:写入日志中……
 echo set: >>"%logs%"
 echo staut up=%su% >>"%logs%"
 echo repair mode=%sfcs% >>"%logs%"
-echo turbo mode=%tb% >>"%logs%"
 echo mrt=%mrts% >>"%logs%"
 echo set end >>"%logs%"
 echo. >>"%logs%"
 echo errors: >>"%logs%"
 echo. >>"%logs%"
-echo Windows Search DisableBackoff errror=%wsdboerr% >>"%logs%"
 echo UAC error=%UACerr% >>"%logs%"
 echo initialization error=%ierr% >>"%logs%"
 echo network-ping error=%nperr% >>"%logs%"
 echo clean error=%cerr% >>"%logs%"
 echo SFC-scan error=%serr% >>"%logs%"
 echo mrt error=%mrterr% >>"%logs%"
-echo memorychip error=%merr% >>"%logs%"
 echo. >>"%logs%"
 echo error end >>"%logs%"
 echo. >>"%logs%"
@@ -448,9 +416,7 @@ echo SCRT-runendtime:%date% %time% >>"%logs%"
 echo SCRT-runtime:%runtime% >>"%logs%"
 echo ---------------------------------------------------------------------------------------------------- >>"%logs%"
 echo ECHO:写入日志成功！
-
-rem SCRTend
-cls&&ver&&echo %name%[版本 %v%]&&echo 制作者：林先生&&echo.
+%cls%
 echo 运行日志在%logs%中
 echo.
 echo UAC自动更改将会在下一次启动时生效！
@@ -470,12 +436,11 @@ echo ECHO:正在退出！
 start /min %tc%
 endlocal
 exit
-
 rem 本文件受个人知识版权保护，但已经开源，使用MIT开源协议，请访问https://github.com/small-lin-jam/SCRT/！
 rem 本文件开源！
 rem 备用代码信息begin
 rem 制作者：林先生。制作团队：Steven Lin Studio（上海林酱工作室）。保留所有权利。
-rem 系统清理诊断程序[版本 8.3.6.6正式版]
+rem 系统清理诊断程序[版本 8.3.7.5正式版]
 rem Steven Lin（林先生）版权所有
 rem 请联系linjunhui2012@hotmail.com获取正版！
 rem 本程序受个人版权保护！
